9f0372efa54210a122c79936699b4790bdfaba4e
davydka <davydka@gmail.com>
2017-04-04 19:33:31 -0400

    Trying out transparencies and background colors.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
+#include "wx/graphics.h"
 class MyApp: public wxApp
 {
 public:
@@ -27,10 +28,12 @@ bool MyApp::OnInit()
 {
     MyFrame *frame = new MyFrame();
     frame->Show( true );
+	printf("cool\n");
     return true;
 }
 MyFrame::MyFrame()
-        : wxFrame(NULL, wxID_ANY, "Hello World")
+        //: wxFrame(NULL, wxID_ANY, "hello application", wxDefaultPosition, wxSize(640, 480), 0 | wxNO_BORDER)
+        : wxFrame(NULL, wxID_ANY, "hello application", wxDefaultPosition, wxSize(640, 480), 0 | wxFRAME_SHAPED)
 {
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
@@ -42,12 +45,24 @@ MyFrame::MyFrame()
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append( menuFile, "&File" );
     menuBar->Append( menuHelp, "&Help" );
-    SetMenuBar( menuBar );
-    CreateStatusBar();
-    SetStatusText( "Welcome to wxWidgets!" );
+    //SetMenuBar( menuBar );
+	//menuBar->Enable(false);
+    //CreateStatusBar();
+    //SetStatusText( "hello hello hello" );
     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+
+	wxGraphicsPath path = wxGraphicsRenderer::GetDefaultRenderer()->CreatePath();
+	//path.AddEllipse(0, 0,GetRect().width-20,GetRect().height/2);
+	path.AddRectangle(0,0,640,448);
+	//path.AddRectangle(0,0,640,480);
+	SetShape(path);
+	/*
+	wxPanel * panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(300, 300));
+	panel->SetBackgroundColour(wxColour(* wxRED));
+	*/
 }
 void MyFrame::OnExit(wxCommandEvent& event)
 {
